4cbb377e2d1850052643336d3ac9166384344c93
tomasz78285 <62183978+tomasz78285@users.noreply.github.com>
2020-05-13 20:08:40 +0200

    Update 42.php

--- 42.php
+++ 42.php
@@ -8,26 +8,16 @@
    
 <?php
 
-        $servername = "localhost";
-        $username = "username";
-        $password = "password";
-        $dbname = "myDB";
+        $mysql_host = 'localhost'; 
+        $username = 'root';
+        $password = '';
+        $database = 'uczelnia';
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $dbh = new PDO('mysql:host=' . $mysql_host . ';dbname=' . $database , $username, $password );
 
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
+        $delete = $dbh->exec("DELETE FROM studenci WHERE id = 2");
 
-        $sql = "DELETE FROM studenci WHERE id = 2";
-
-        if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-        $conn->close();
+        
    ?>
 
 </body>
